41c3a513bbdd570ab9ffb39b3799ad60e18b677a
unknown <ismail.edaraz@gmail.com>
2023-11-30 22:43:41 +0100

    Add router

---
+++ Router/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Router;
+
+use Exceptions\RouteNotFoundException;
+
+class Router
+{
+    private array $routes;
+
+    public function register(string $path, callable $action): void
+    {
+        $this->routes[$path] = $action;
+        var_dump($action);
+    }
+
+    public function resolve(string $uri): mixed
+    {
+        $path = explode('?', $uri)[0];
+        $action = $this->routes[$path] ?? null;
+
+        if (!is_callable($action)) {
+            throw new RouteNotFoundException();
+        }
+
+        return $action();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
 
-require '../vendor/autoload.php';
+use Exceptions\RouteNotFoundException;
+use Router\Router;
 
-echo "Hello";
+require './../vendor/autoload.php';
+
+$router = new Router();
+
+$router->register('/', function () {
+    return 'HomePage';
+});
+
+$router->register('/contact', function () {
+    return 'Contact Page';
+});
+
+try {
+    echo $router->resolve($_SERVER['REQUEST_URI']);
+} catch (RouteNotFoundException $e) {
+    echo $e->getMessage();
+}
